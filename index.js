5a5096481d501768ffe09e76ee31e60a459250a0
amralashi <94717469+amralashi@users.noreply.github.com>
2023-01-27 01:26:46 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Simple linked list class
 class Node {
   constructor(value){
     this.value = value;
@@ -5,6 +6,8 @@ class Node {
   }
 }
 
+
+//Stack class
 class Stack {
   constructor(){
     
@@ -12,9 +15,15 @@ class Stack {
     this.bottom = null;
     this.length = 0;
   }
+
+  //Gets the top node of the stack
   peek() {
     console.log(this.top);
   }
+
+  //Push a new value to the top of the stack
+  //The pointers of the list are connected from top to bottom
+  //this.top --> node --> node --> ... --> this.bottom
   push(value){
 
     const node = new Node(value);
@@ -33,46 +42,41 @@ class Stack {
     this.length++;
     console.log(this);
   }
+
+  //Pop the top node out of the stack
   pop(){
 
     const holdingPointer = this.top;
     this.top = this.top.next;
     this.length--;
 
-    if(this.length === 0){
+    if(this.length === 0){ // In case of removing the last node, clear the this.bottom
     this.bottom = null;
     }
     
     console.log(this);
-  
   }
 
-
+//Checks if the stack is empty
   isEmpty(){
     console.log(this.length === 0);
   }
 }
 
+
+//Call code starts from here
 const myStack = new Stack();
 myStack.push('Discord');
 myStack.push('Udemy');
 myStack.push('Google');
 
-
+myStack.isEmpty();
 
 myStack.pop();
 myStack.pop();
 myStack.pop();
-
-
-
-myStack.peek();
 
 myStack.isEmpty();
 
-myStack.push('Google');
-myStack.push('Amazon');
 myStack.peek();
-//Discord
-//Udemy
-//google
+
